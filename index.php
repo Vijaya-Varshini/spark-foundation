5d707e7fe53aab8fd38ed947d33da948c6e25deb
Vijaya-Varshini <100292844+Vijaya-Varshini@users.noreply.github.com>
2022-02-23 19:40:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('https://www.thebalance.com/thmb/39p9AgpjFyHIYEdwW8BWLyKZkik=/1500x998/filters:no_upscale():max_bytes(150000):strip_icc()/how-much-money-should-i-be-saving-59756a1dc4124400114e3c29-5bf3391ac9e77c0051fc89cd.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    background-size: cover;
+    background-position: cover;
+  
+  }
+  ul{
+    float:right;
+    list-style-type: none;
+    margin-top: 30px;
+  }
+  ul li{
+    display:inline-block;
+  }
+  ul li a{
+    text-decoration: none;
+    color: ggg;
+    padding:10px 15px;
+    border:1px solid #fff;
+  }
+  ul li a:hover{
+    background-color:aaa;
+    color:#000;
+  }
+  .title{
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-50%);
+  }
+  .title h1{
+    color:#ggg;
+    font-size:70px;
+    
+  }
+  }
+</style>
+<v1 style = "position:relative; left:950px; top:100px; color:black;  font-family: Comic Sans MS;">W</h1></b>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">E</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">L</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">C</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">O</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">M</h1>
+<v1 style = "position:relative; left:0px; top:0px; color:black;  font-family: Comic Sans MS;">E</h1>
+<footer class="text-center mt-5 py-2"><p>&copy<b> Designed by Gayathri U, Web Development Intern <br> @ Sparks Foundation</p></b></footer>
+    </main>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : aa;">
+        <div class="title"><b><h1>SPARKS BANK</h1></b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="active">
+                <a class="nav-link" href="index.php" style="grid-row-end: aa;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="users.php" style="grid-row-end : #38adda;"><b>users</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="grid-row-end :#38adda;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="grid-row-end : #38adda;"><b>Transaction History</b></a>
+              </li>
+              
+          </div>
+       </nav>
